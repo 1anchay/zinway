1e4beefaace7795f26903e367c50912c603c8d05
ivan zhivo <vtar4a@gmail.com>
2026-05-13 12:21:10 +0300

    Fix profile tailwind

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Профиль | Курс молодого бойца VR</title>
-
+    <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@500;600;700&family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
 
     <style>
